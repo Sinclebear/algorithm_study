3ed205325969cf1f9b37e91dc1ae79ac7ef3a51a
Sinclebear <sanghyukj@gmail.com>
2022-03-17 12:49:36 +0900

    🚀 백준 11399 ATM 풀이

--- algorithm_baekjoon/chapter3_정수론_그리디_브루트포스/11399_ATM.js
+++ algorithm_baekjoon/chapter3_정수론_그리디_브루트포스/11399_ATM.js
@@ -3,8 +3,6 @@
 // let input = require('fs').readFileSync('./dev/stdin').toString().trim().split('\n');
 // let [b,c] = input[0].split(' ').map(x=>+x);
 
-const { time } = require("console");
-
 let input = require("fs")
   .readFileSync("./dev/stdin")
   .toString()
