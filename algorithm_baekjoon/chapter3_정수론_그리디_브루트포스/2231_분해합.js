759926de90ea6933dfd9b9807bc508d3da8cd70b
Sinclebear <sanghyukj@gmail.com>
2022-03-18 04:31:19 +0900

    🚀 백준 2231 분해합 풀이

---
+++ algorithm_baekjoon/chapter3_정수론_그리디_브루트포스/2231_분해합.js
@@ -0,0 +1,39 @@
+// https://www.acmicpc.net/problem/2231 백준 : 분해합
+//표준 입출력 구현
+// let input = require('fs').readFileSync('./dev/stdin').toString().trim().split('\n');
+// let [b,c] = input[0].split(' ').map(x=>+x);
+
+let input = require("fs")
+  .readFileSync("./dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+const number = parseInt(input);
+let result = 1;
+let answer_flag = 0; // 생성자가 없는 경우를 구분하기 위한 flag값 0으로 설정.
+
+// 분해합 구하는 함수 실행
+function DigitGenerator() {
+  // 주어진 입력값 number만큼 for문 수행
+  for (let i = 1; i < number; i++) {
+    result = i;
+
+    const stringValue = i.toString(); // 자릿수 덧셈을 위해 문자열로 변경
+
+    //String이 배열과 유사한점을 사용하여, String.length, String[j] 를 활용해 각 자릿수를 더한다.
+    for (let j = 0; j < stringValue.length; j++) {
+      result += parseInt(stringValue[j]);
+    }
+
+    // 생성자의 분해합이 주어진 값과 일치하는지 지속해서 체크
+    if (result === number) {
+      console.log(i); // 현재 만들어진 생성자 값 출력
+      answer_flag = 1; // answer_flag 값 변경하여 마지막 부분에서 console.log(0) 실행한되도록 변경
+      return;
+    }
+  }
+}
+
+DigitGenerator();
+if (answer_flag === 0) console.log(0);
